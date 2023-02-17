36e641d660c65d624caae8d446c312a0fa9952d1
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-02-17 14:32:49 +0100

    feat: fact layout

---
+++ layouts/fact.vue
@@ -0,0 +1,16 @@
+<template>
+  <Themeify
+    class="slidev-layout fact"
+    w:grid="~"
+    w:place="content-center"
+    w:text="center"
+  >
+    <slot />
+  </Themeify>
+</template>
+
+<style>
+.slidev-layout.fact h1 {
+  color: var(--slidev-theme-primary);
+}
+</style>
